9a4f3c04260361abb90ff878d6c4616057a3183e
DeniD-star <dimitrova.denise@gmail.com>
2022-11-07 22:14:45 +0100

    using ngfor directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  title = 3;
 
-  title = 1;
-  changeTitle(){
-    this.title ++;
+  users = [
+    { name: 'Ivan 1',
+     age: 21 },
+    {
+      name: 'Ivan 2',
+      age: 22,
+    },
+    {
+      name: 'Ivan 3',
+      age: 23,
+    },
+  ];
+  changeTitle(): void {
+    // this.title ++;
+    const current = this.title++;
 
+    this.users.push({
+      name: `Ivan ${current}`,
+      age: 20 + current
+    })
   }
 }
