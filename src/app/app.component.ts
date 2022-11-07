a90d46bc77c3aeb6b7870d8529191a102d7ca328
DeniD-star <dimitrova.denise@gmail.com>
2022-11-07 22:28:32 +0100

    knowing ngif directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,27 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 3;
+  // title = 3;
 
-  users = [
-    { name: 'Ivan 1',
-     age: 21 },
-    {
-      name: 'Ivan 2',
-      age: 22,
-    },
-    {
-      name: 'Ivan 3',
-      age: 23,
-    },
-  ];
-  changeTitle(): void {
-    // this.title ++;
-    const current = this.title++;
+  // users = [
+  //   { name: 'Ivan 1',
+  //    age: 21 },
+  //   {
+  //     name: 'Ivan 2',
+  //     age: 22,
+  //   },
+  //   {
+  //     name: 'Ivan 3',
+  //     age: 23,
+  //   },
+  // ];
+  // changeTitle(): void {
+  //   // this.title ++;
+  //   const current = this.title++;
 
-    this.users.push({
-      name: `Ivan ${current}`,
-      age: 20 + current
-    })
+  //   this.users.push({
+  //     name: `Ivan ${current}`,
+  //     age: 20 + current
+  //   })
+  // }
+
+
+  showText = true;
+  toggleText(): void{
+    this.showText = !this.showText;
   }
+
+
 }
